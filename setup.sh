c47250dd35c6330b2b9118a52d7ced78bcd3f655
shaneorourke <shaneorourke110@gmail.com>
2023-03-16 20:26:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 chmod +x sma_cross.sh &&
-chmod +x sma_cross_while_loop.sh &&
+#chmod +x sma_cross_while_loop.sh &&
 mkdir /home/shane/Documents/cronlogs -p &&
 python3 -m venv env &&
 source env/bin/activate &&
